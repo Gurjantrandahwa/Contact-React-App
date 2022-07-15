30ffd15f9a134e1893aed137cda2e7f17bcf3486
Gurjant Singh <gurjantrandhawa74456@gmail.com>
2022-07-15 13:32:36 +0530

    contact-app implemented

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
+import React, {useState, useEffect} from "react";
 import './App.css';
+import Header from "./Components/Header";
+import ContactList from "./Components/ContactList";
+import AddContact from "./Components/AddContact";
+import ContactDetail from "./Components/ContactDetail";
+import {BrowserRouter, Route, Routes} from 'react-router-dom';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    const [contacts, setContacts] = useState([]);
+
+    const addContactHandler = (contact) => {
+        setContacts([...contacts, contact])
+        localStorage.setItem('contacts',JSON.stringify([...contacts, contact]))
+    };
+    const removeContactHandler = (id) => {
+        const newContactList = contacts.filter((contact) => {
+            return contact.id !== id;
+        });
+        localStorage.setItem('contacts',JSON.stringify(newContactList))
+        setContacts(newContactList)
+    }
+
+
+    useEffect(() => {
+        const retrieveContacts = JSON.parse(localStorage.getItem('contacts'))
+        console.log(retrieveContacts)
+        if (retrieveContacts) setContacts(retrieveContacts);
+    }, []);
+
+    return <div>
+        <BrowserRouter>
+            <Header/>
+            <Routes>
+                <Route path="/add"
+                       element={<AddContact
+                           addContactHandler={addContactHandler}/>}
+                />
+                <Route path="/contact/:id"
+                       element={<ContactDetail
+                           contacts={contacts}
+                       />}/>
+                <Route path="/"
+                       element={ <ContactList
+                           contacts={contacts}
+                           getContactID={removeContactHandler}/>}/>
+            </Routes>
+        </BrowserRouter>
     </div>
-  );
 }
 
 export default App;
